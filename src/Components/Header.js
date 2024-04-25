f3133b8bd750f180ad061f7b8419d51d4ab2d6a3
VidyaSagarMehar <vidyasagark890@gmail.com>
2024-04-25 23:04:27 +0530

    Bug fix - Moved the reirecting after login/signup to the header(central place)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { signOut } from 'firebase/auth';
 import { auth } from '../utils/firebase';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useDispatch } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
 
 const Header = () => {
 	const navigate = useNavigate();
 	const user = useSelector((store) => store.user);
+	const dispatch = useDispatch();
 
 	const handleSignOut = () => {
 		signOut(auth)
 			.then(() => {
 				// Sign-out successful.
-				navigate('/');
 			})
 			.catch((error) => {
 				// An error happened.
 				navigate('/error');
 			});
 	};
+
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				// User is signed in
+				const { uid, email, displayName, photoURL } = user;
+				dispatch(
+					addUser({
+						uid: uid,
+						email: email,
+						displayName: displayName,
+						photoURL: photoURL,
+					}),
+				);
+				navigate('/browse');
+			} else {
+				dispatch(removeUser());
+				navigate('/');
+			}
+		});
+	}, []);
 
 	return (
 		<div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-full flex justify-between">
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -7,14 +7,12 @@ import {
 	updateProfile,
 } from 'firebase/auth';
 import { auth } from '../utils/firebase';
-import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addUser } from '../utils/userSlice';
 
 const Login = () => {
 	const [isSignInForm, setIsSignInForm] = useState(true);
 	const [errorMessage, setErrorMessage] = useState(null);
-	const navigate = useNavigate();
 	const dispatch = useDispatch();
 
 	const name = useRef(null);
@@ -57,7 +55,6 @@ const Login = () => {
 								}),
 							);
 							console.log(user);
-							navigate('/browse');
 						})
 						.catch((error) => {
 							// An error occurred
@@ -79,8 +76,7 @@ const Login = () => {
 			)
 				.then((userCredential) => {
 					const user = userCredential.user;
-					console.log(user);
-					navigate('/browse');
+					// console.log(user);
 				})
 				.catch((error) => {
 					const errorCode = error.code;
