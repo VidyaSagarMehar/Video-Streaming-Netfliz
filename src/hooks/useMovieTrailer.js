2e938b621b5c14bc7e8920303240b4f56a82cc57
VidyaSagarMehar <vidyasagark890@gmail.com>
2024-06-05 11:30:41 +0530

    moving fetch and useEffect in a separate custome Hooks

---
+++ src/hooks/useMovieTrailer.js
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import { API_OPTIONS } from '../utils/constant';
+import { useDispatch } from 'react-redux';
+import { addTrailerVideo } from '../utils/moviesSlice';
+
+const useMovieTrailer = () => {
+	const dispatch = useDispatch();
+
+	const getMovieVideos = async () => {
+		const data = await fetch(
+			'https://api.themoviedb.org/3/movie/653346/videos?language=en-US',
+			API_OPTIONS,
+		);
+		const json = await data.json();
+		console.log(json);
+		const filterData = json.results.filter((video) => video.type === 'Trailer');
+		const trailer = filterData.length ? filterData[0] : json.results[0];
+		console.log(trailer);
+		dispatch(addTrailerVideo(trailer));
+	};
+
+	useEffect(() => {
+		getMovieVideos();
+	}, []);
+};
+
+export default useMovieTrailer;
